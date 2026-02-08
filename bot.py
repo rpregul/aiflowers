98ab824f9ac9147dfb87c1a22daa0b38d1bc93f6
rpregul <rp.regul@gmail.com>
2026-02-08 17:25:13 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 OPENROUTER_URL = "https://openrouter.ai/api/v1/chat/completions"
-model: "black-forest-labs/flux.2-pro"
+MODEL = "black-forest-labs/flux.2-pro"
 
 async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
